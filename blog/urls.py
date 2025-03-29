e0782aeb7e43cfb9f5ac092837802d83b3f3c64e
dr.restful cod <dr.restful@gmail.com>
2025-03-29 22:01:16 +0100

    blog detail

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<pk>/', views.blog_detail, name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .models import Blog
+from django.contrib.auth.decorators import login_required
 
+
+# prikaz svih postova
 def home(request):
     blog = Blog.objects.all()
-    
-
     return render(request, 'blog/home.html', {'blogs': blog})
+
+# prikaz detalja posta
+@login_required
+def blog_detail(request, pk):
+    blog = Blog.objects.get(pk=pk)  
+    return render(request, 'blog/blog_detail.html', {'blog': blog})
